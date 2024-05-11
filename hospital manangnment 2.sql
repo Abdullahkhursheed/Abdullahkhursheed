19c445333f4d98121db6347e6f60550d018b3937
Abdullahkhursheed <abdullah.khursheed2112@gmail.com>
2024-05-11 17:45:14 +0500

    Add files via upload

---
+++ hospital manangnment 2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `patient` (
+  `patientname` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `phone number` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  PRIMARY KEY (`address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SELECT * FROM hospitalmanagment.patient;
