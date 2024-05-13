bdd3b502833748383fee3f869a26270b159ba9ea
Abdullahkhursheed <abdullah.khursheed2112@gmail.com>
2024-05-13 20:40:18 +0500

    Delete hospital manangnment.sql

--- hospital manangnment.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM hospitalmanagment.depatments;
-CREATE TABLE `depatments` (
-  `Depatments names` varchar(45) NOT NULL,
-  `Floors` int NOT NULL,
-  `Rooms` int NOT NULL,
-  PRIMARY KEY (`Floors`,`Rooms`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
