36c3faa77e6b2e9835e46f2156e4cf4592a26b4f
Abdullahkhursheed <abdullah.khursheed2112@gmail.com>
2024-05-13 20:39:41 +0500

    Delete hospital managnment.sql

--- hospital managnment.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM hospitalmanagment.patient;CREATE TABLE `appointments` (
-  `patientname` varchar(45) NOT NULL,
-  `dostorsname` varchar(45) NOT NULL,
-  `Time` int NOT NULL,
-  `Date` int NOT NULL,
-  `specialization` varchar(45) NOT NULL,
-  PRIMARY KEY (`Time`,`Date`,`specialization`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
