7cd16a6688eb07ec32fdbf28a337e13f790f5105
Abdullahkhursheed <abdullah.khursheed2112@gmail.com>
2024-05-13 20:49:29 +0500

    Update hospitalsystem.sql

--- hospitalsystem.sql
+++ hospitalsystem.sql
@@ -6,21 +6,21 @@ CREATE TABLE `appointments` (
   `Date` int NOT NULL,
   `specialization` varchar(45) NOT NULL,
   PRIMARY KEY (`Time`,`Date`,`specialization`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) 
 SELECT * FROM hospitalmanagment.depatments;
 CREATE TABLE `depatments` (
   `Depatments names` varchar(45) NOT NULL,
   `Floors` int NOT NULL,
   `Rooms` int NOT NULL,
   PRIMARY KEY (`Floors`,`Rooms`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) 
 SELECT * FROM hospitalmanagment.doctors;
 CREATE TABLE `doctors` (
   `name` varchar(45) NOT NULL,
   `specialization` varchar(45) NOT NULL,
   `phone number` int NOT NULL,
   PRIMARY KEY (`phone number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) 
 SELECT * FROM hospitalmanagment.patient;
 CREATE TABLE `patient` (
   `patientname` varchar(45) NOT NULL,
@@ -28,4 +28,4 @@ CREATE TABLE `patient` (
   `phone number` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
   PRIMARY KEY (`address`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) 
