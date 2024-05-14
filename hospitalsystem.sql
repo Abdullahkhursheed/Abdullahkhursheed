82c0a29037ded4bd466caf560d0ddb932f61ee92
umershakeel928 <umershakeel928@gmail.com>
2024-05-14 23:37:07 +0500

    Update hospitalsystem.sql
    this file is edit by umer shakil

--- hospitalsystem.sql
+++ hospitalsystem.sql
@@ -29,3 +29,25 @@ CREATE TABLE `patient` (
   `address` varchar(45) NOT NULL,
   PRIMARY KEY (`address`)
 ) 
+SELECT * FROM `medical history`.`medical history`;
+CREATE TABLE `medical history` (
+  `patient_id` int NOT NULL,
+  `   patient_name` varchar(45) NOT NULL,
+  `     date _of_visit` varchar(45) NOT NULL,
+  `          diagonosis` varchar(45) NOT NULL,
+  `             Treatment` varchar(45) NOT NULL,
+  PRIMARY KEY (`patient_id`,`          diagonosis`,`             Treatment`,`     date _of_visit`)
+) 
+CREATE TABLE `prescription` (
+  `PrescriptionId` int NOT NULL,
+  `   RecordID` int NOT NULL,
+  ` MedicineID` int NOT NULL,
+  PRIMARY KEY (`PrescriptionId`)
+)
+SELECT * FROM `medical history`.`staff table`;
+CREATE TABLE `staff table` (
+  `stafID` int NOT NULL,
+  ` name` varchar(45) NOT NULL,
+  `  role` varchar(45) NOT NULL,
+  PRIMARY KEY (`stafID`,` name`)
+)
